ed488e0928d4751d5fd9599b26b65b637c62dec7
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-02 18:07:16 +0100

    (main) create world from files

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	ghashcode "./ghashcode"
+	config "./config"
+	simulator "./simulator"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
@@ -20,6 +21,9 @@ var (
 
 	frames = 0
 	second = time.Tick(time.Second)
+
+	outputFile *string
+	inputFile  *string
 )
 
 func createGrid(sizeX, sizeY int32) *imdraw.IMDraw {
@@ -27,14 +31,14 @@ func createGrid(sizeX, sizeY int32) *imdraw.IMDraw {
 
 	imd.Color = colornames.Blueviolet
 	imd.EndShape = imdraw.RoundEndShape
-	squareSize := int32(5)
+	squareSize := config.Config.UI.SquareSize
 	for x := int32(0); x < sizeX; x++ {
 		for y := int32(0); y < sizeY; y++ {
 			offsetX := x*squareSize + squareSize
 			offsetY := y*squareSize + squareSize
 			imd.Push(pixel.V(float64(x+offsetX), float64(y+offsetY)))
 			imd.Push(pixel.V(float64(x+squareSize+offsetX), float64(y+squareSize+offsetY)))
-			imd.Rectangle(2)
+			imd.Rectangle(1)
 		}
 	}
 
@@ -43,12 +47,18 @@ func createGrid(sizeX, sizeY int32) *imdraw.IMDraw {
 
 func init() {
 	frameRate = flag.Duration("frameRate", 33*time.Millisecond, "The framerate in milliseconds")
+	outputFile = flag.String("o", "", "Path to your result")
+	inputFile = flag.String("i", "", "Path to the exercice input")
 	flag.Parse()
 }
 
 func run() {
+	vehicles := simulator.ParseOutputFile(*outputFile)
+	trips := simulator.ParseInputFile(*inputFile)
+	fmt.Printf("%v", trips[0])
+
 	cfg := pixelgl.WindowConfig{
-		Title:  "Google Hashcode 2018 - Simulator!",
+		Title:  config.Config.UI.WindowTitle,
 		Bounds: pixel.R(0, 0, 1024, 768),
 	}
 	win, err := pixelgl.NewWindow(cfg)
@@ -56,15 +66,7 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Skyblue)
-
 	grid := createGrid(100, 100)
-	vehicle := new(ghashcode.Vehicle)
-	vehicle.SetPosition(1, 3)
-
-	trip := new(ghashcode.Trip)
-	trip.Start = ghashcode.Coordinates{50, 50}
-	trip.End = ghashcode.Coordinates{20, 80}
 
 	tick := time.Tick(*frameRate)
 	for !win.Closed() {
@@ -72,9 +74,14 @@ func run() {
 		frames++
 		select {
 		case <-tick:
+			win.Clear(colornames.Whitesmoke)
 			grid.Draw(win)
-			vehicle.DrawToWindow(win)
-			trip.DrawToWindow(win)
+			for _, vehicle := range vehicles {
+				vehicle.DrawToWindow(win)
+			}
+			for _, trip := range trips {
+				trip.DrawToWindow(win)
+			}
 		case <-second:
 			win.SetTitle(fmt.Sprintf("%s | FPS: %d", cfg.Title, frames))
 			frames = 0

2852d7a72bd79d96f66ca2bcec8dc96f70fb54da
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-02 15:30:49 +0100

    (all) display fps in title

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"time"
+
 	ghashcode "./ghashcode"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
+)
+
+var (
+	size       *int
+	windowSize *float64
+	frameRate  *time.Duration
+
+	frames = 0
+	second = time.Tick(time.Second)
 )
 
 func createGrid(sizeX, sizeY int32) *imdraw.IMDraw {
@@ -26,6 +39,11 @@ func createGrid(sizeX, sizeY int32) *imdraw.IMDraw {
 	}
 
 	return imd
+}
+
+func init() {
+	frameRate = flag.Duration("frameRate", 33*time.Millisecond, "The framerate in milliseconds")
+	flag.Parse()
 }
 
 func run() {
@@ -48,10 +66,19 @@ func run() {
 	trip.Start = ghashcode.Coordinates{50, 50}
 	trip.End = ghashcode.Coordinates{20, 80}
 
+	tick := time.Tick(*frameRate)
 	for !win.Closed() {
-		grid.Draw(win)
-		vehicle.DrawToWindow(win)
-		trip.DrawToWindow(win)
+		// logic loop
+		frames++
+		select {
+		case <-tick:
+			grid.Draw(win)
+			vehicle.DrawToWindow(win)
+			trip.DrawToWindow(win)
+		case <-second:
+			win.SetTitle(fmt.Sprintf("%s | FPS: %d", cfg.Title, frames))
+			frames = 0
+		}
 		win.Update()
 	}
 }

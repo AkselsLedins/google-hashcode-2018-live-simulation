1e0df6cab68fd08d112d83b4d1af1da93b17635e
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-03 10:40:27 +0100

    (vehicle) +DriveTo +GetPosition +Drive

--- ghashcode/vehicle.go
+++ ghashcode/vehicle.go
@@ -1,6 +1,8 @@
 package ghashcode
 
 import (
+	"math"
+
 	config "../config"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
@@ -12,6 +14,7 @@ type Vehicle struct {
 
 	CurrentRide int
 	OnRide      bool
+	Trips       []int32
 }
 
 func (v *Vehicle) DrawToWindow(win *pixelgl.Window) {
@@ -19,17 +22,18 @@ func (v *Vehicle) DrawToWindow(win *pixelgl.Window) {
 
 	imd.Color = config.Config.UI.VehicleDefaultColor
 	imd.EndShape = imdraw.RoundEndShape
-	squareSize := int32(5)
+	squareSize := config.Config.UI.SquareSize
 
 	offsetX := v.CurrentPosition.X*squareSize + squareSize
 	offsetY := v.CurrentPosition.Y*squareSize + squareSize
 
-	drawX := float64(v.CurrentPosition.X + offsetX)
-	drawY := float64(v.CurrentPosition.Y + offsetY)
+	drawX := float64(offsetX)
+	drawY := float64(offsetY)
+
 	imd.Push(pixel.V(drawX, drawY))
 	imd.Push(pixel.V(drawX, drawY))
 
-	imd.Line(2)
+	imd.Line(config.Config.UI.VehicleSize)
 
 	imd.Draw(win)
 }
@@ -37,4 +41,57 @@ func (v *Vehicle) DrawToWindow(win *pixelgl.Window) {
 func (v *Vehicle) SetPosition(x, y int32) {
 	v.CurrentPosition.X = x
 	v.CurrentPosition.Y = y
+}
+
+func (v *Vehicle) Drive(allTrips []*Trip) {
+	// it has done every of his trips
+	if v.CurrentRide > len(v.Trips) {
+		return
+	}
+
+	tripToGoTo := allTrips[v.Trips[v.CurrentRide]]
+	tripToGoTo.SomeoneIsOnIt()
+	v.DriveTo(tripToGoTo.Start.X, tripToGoTo.Start.Y)
+}
+
+func (v *Vehicle) GetPosition() (int32, int32) {
+	return v.CurrentPosition.X, v.CurrentPosition.Y
+}
+
+func (v *Vehicle) DriveTo(x, y int32) {
+	cx, cy := v.GetPosition()
+	dx := math.Abs(float64(cx - x))
+	dy := math.Abs(float64(cy - y))
+
+	// fmt.Printf("goto %d %d\n", x, y)
+	nx := cx
+	ny := cy
+	if dx < dy {
+		if cy < y {
+			ny++
+		} else if cy > y {
+			ny--
+		}
+	} else if dx > dy {
+		if cx < x {
+			nx++
+		} else if cx > x {
+			nx--
+		}
+	} else if dx == dy {
+		if cx < x {
+			nx++
+		} else if cx > x {
+			nx--
+		} else if cy < y {
+			ny++
+		} else if cy > y {
+			ny--
+		}
+	}
+	// fmt.Printf("driving to %d, %d\n", x, y)
+	// fmt.Printf("current pos [%d, %d]", v.CurrentPosition.X, v.CurrentPosition.Y)
+	// fmt.Printf("-\n")
+	// fmt.Printf("moving from (%d, %d) to (%d, %d)\n", cx, cy, nx, ny)
+	v.SetPosition(nx, ny)
 }
